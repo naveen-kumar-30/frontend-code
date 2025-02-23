ab651dbda4b2d747197def2a027dd05f402bac3a
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-02-23 11:34:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ export default defineConfig({
   optimizeDeps: {
     include: ["@monaco-editor/react"],
     exclude: ["monaco-editor"]
+  },
+  build: {
+    commonjsOptions: {
+      ignoreDynamicRequires: true, // To avoid issues with dynamic imports.
+    }
   }
 });
