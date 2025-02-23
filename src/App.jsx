2173dc6d632571f3f9de1c0d1f4cf96659f10ef5
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-02-23 11:45:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';  
+import Home from './pages/home';  
 import Editor from "./pages/Editor";
 import Login from "./pages/login";
 import CollaborativeEditor from "./pages/CollaborativeEditor"; // Import Collaborative Editor
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<home />} />
         <Route path="/editor" element={<Editor />} />
         <Route path="/login" element={<Login />} />
         <Route path="/collaborative-editor/:roomId" element={<CollaborativeEditor />} />
